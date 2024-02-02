7facc7f4634670d5de280c516af299bb6dd6d119
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-02 09:56:00 -0600

    update inglist component to use hooks

--- src/IngList.js
+++ src/IngList.js
@@ -2,33 +2,32 @@
 // each iteration of the loop will display one clickable component
 // this clickable item will be rendered by another component, called Ingredient
 
-import React, { Component } from "react";
 import Ingredient from "./Ingredient";
 
-export default class IngList extends Component {
-    render () {
-        // Use destructuring syntax to isolate anything brought in from props
-        const { ingredients } = this.props;
-        // console.log('the props in IngList', ingredients);
+const IngList = (props) => {
 
-        let allIngs = ingredients.map((ing, i) => (
-            // this map will immediately return one item per loop iteration
-            <li key={i}>
-                <Ingredient 
-                    ingredient={ing}
-                    clickFunc={this.props.add}
-                    itemKey={i}
-                />
-            </li>
-        ));
+    // Use destructuring syntax to isolate anything brought in from props
+    const { ingredients } = props;
 
-        return (
-            <section class="pane">
-                <h3>Ingredient List</h3>
-                <ul>
-                    { allIngs }
-                </ul>
-            </section>
-        );
-    }
-}
+    let allIngs = ingredients.map((ing, i) => (
+        // this map will immediately return one item per loop iteration
+        <li key={i}>
+            <Ingredient 
+                ingredient={ing}
+                clickFunc={props.add}
+                itemKey={i}
+            />
+        </li>
+    ));
+
+    return (
+        <section class="pane">
+            <h3>Ingredient List</h3>
+            <ul>
+                { allIngs }
+            </ul>
+        </section>
+    );
+};
+
+export default IngList;
