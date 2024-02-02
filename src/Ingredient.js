b7206fef6edfbb2350595f0617b3a7512af13433
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-02 09:50:54 -0600

    update ingredient component to use hooks

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -7,24 +7,20 @@
 // if the component is rendered by BurgerPane
 // then the click will remove it from the burger
 
-import React, { Component } from "react";
+const Ingredient = (props) => {
 
-export default class Ingredient extends Component {
+    // destructure our ingredient k:v pairs for easy reference
+    const { name, color } = props.ingredient;
 
-    render () {
-        // console.log('this is props in Ingredient', this.props);
-        // destructure our ingredient k:v pairs for easy reference
-        const { name, color } = this.props.ingredient;
+    return (
+        <p
+            style={{backgroundColor: color}}
+            onClick={props.clickFunc}
+            id={props.itemKey}
+        >
+            { name }
+        </p>
+    );
+};
 
-
-        return (
-            <p
-                style={{backgroundColor: color}}
-                onClick={this.props.clickFunc}
-                id={this.props.itemKey}
-            >
-                { name }
-            </p>
-        );
-    }
-}
+export default Ingredient;
