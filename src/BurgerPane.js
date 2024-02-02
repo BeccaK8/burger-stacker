52c469b06d3c72a56f75a3edc1aee98e7e931669
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-02 09:57:33 -0600

    update burgerPane component to use hooks

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -2,32 +2,31 @@
 // each iteration of the loop will display one clickable component
 // this clickable item will be rendered by another component, called Ingredient
 
-import React, { Component } from "react";
 import Ingredient from "./Ingredient";
 
-export default class BurgerPane extends Component {
-    render () {
-        //console.log('the props in BurgerPane', this.props);
-        // Use destructuring syntax to isolate anything brought in from props
-        const { ingredients } = this.props;
-        
-        let burgerBits = ingredients.map((ing, i) => (
-            <li key={i}>
-                <Ingredient 
-                    ingredient={ing}
-                    clickFunc={this.props.remove}
-                    itemKey={i}
-                />
-            </li>
-        ));
-        return (
-            <section class="pane">
-                <h3>Burger Stack</h3>
-                <ul>
-                    { burgerBits }
-                </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
-            </section>
-        );
-    }
-}
+const BurgerPane = (props) => {
+
+    // Use destructuring syntax to isolate anything brought in from props
+    const { ingredients } = props;
+    
+    let burgerBits = ingredients.map((ing, i) => (
+        <li key={i}>
+            <Ingredient 
+                ingredient={ing}
+                clickFunc={props.remove}
+                itemKey={i}
+            />
+        </li>
+    ));
+    return (
+        <section class="pane">
+            <h3>Burger Stack</h3>
+            <ul>
+                { burgerBits }
+            </ul>
+            <button onClick={props.clear}>Clear Burger</button>
+        </section>
+    );
+};
+
+export default BurgerPane;
